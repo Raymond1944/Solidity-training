33315621636919093efefdd8c1289d5806a73a7e
Raymond1944 <70814729+Raymond1944@users.noreply.github.com>
2024-05-06 15:42:36 +0100

    Create bytesExample.sol

---
+++ bytesExample.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.18;
+
+contract Bytes{
+
+    bytes3 a = 0xaf6473;
+    bytes2 b = 0xeeff;
+    bytes1 c = 0xab;
+
+    bytes5 z = "water";
+    bytes4 y = "rice";
+    bytes10 x = "eaglestone";
+
+}
+/// Each of the character in hexadecimal represent 4 bits
+//Each of the string character represents 8 bits
+// recall 1 bytes = 8 bits
